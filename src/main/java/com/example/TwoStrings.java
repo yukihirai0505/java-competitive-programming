8c4796ddd05557bba67ff3a718fe9b178c1e9656
Yuki Hirai <yukihirai0505@gmail.com>
2018-09-27 15:41:34 +0900

    Add TwoStrings

---
+++ src/main/java/com/example/TwoStrings.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class TwoStrings {
+    // Complete the twoStrings function below.
+    private static String twoStrings(String s1, String s2) {
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < s1.length(); i++) {
+            set.add(s1.charAt(i));
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
+            if (set.contains(c)) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
