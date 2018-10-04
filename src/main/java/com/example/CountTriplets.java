38111341a6afe17f85e6d99c05617cabf3f848ed
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-04 16:50:39 +0900

    Fix CountTriplets

--- src/main/java/com/example/CountTriplets.java
+++ src/main/java/com/example/CountTriplets.java
@@ -1,61 +1,39 @@
 package com.example;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CountTriplets {
 
     // Complete the countTriplets function below.
     private static long countTriplets(List<Long> arr, long r) {
-        HashSet<int[]> set = new HashSet<>();
-        int length = arr.size();
-        for (int i = 0; i < length - 2; i++) {
-            Long secondNum = arr.get(i) * r;
-            Long thirdNum = secondNum * r;
-            for (int j = i + 1; j < length; j++) {
-                if (secondNum.equals(arr.get(j))) {
-                    for (int k = j + 1; k < length; k++) {
-                        if (thirdNum.equals(arr.get(k))) {
-                            int[] comb = {i, j, k};
-                            set.add(comb);
-                        }
-                    }
+        long count = 0;
+        Map<Long, Long> oneCounts = new HashMap<>();
+        Map<Long, Long> pairCounts = new HashMap<>();
+        for (Long num : arr) {
+            if (num % r == 0) {
+                long pre = num / r;
+                Long pairCount = pairCounts.get(pre);
+                if (pairCount != null) {
+                    count += pairCount;
+                }
+                Long cnt1 = oneCounts.get(pre);
+                if (cnt1 != null) {
+                    pairCounts.put(num, pairCounts.getOrDefault(num, 0L) + cnt1);
                 }
             }
+            oneCounts.put(num, oneCounts.getOrDefault(num, 0L) + 1);
         }
-        return set.size();
+        return count;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int n = Integer.parseInt(nr[0]);
-
-        long r = Long.parseLong(nr[1]);
-
-        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Long::parseLong)
-                .collect(toList());
-
-        long ans = countTriplets(arr, r);
-
-        bufferedWriter.write(String.valueOf(ans));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+    public static void main(String[] args) {
+        //long arr[] = {1,2,2,4};
+        Long[] arr = new Long[]{1L, 3L, 9L, 9L, 27L, 81L};
+        List<Long> list = Arrays.asList(arr);
+        int r = 3;
+        CountTriplets.countTriplets(list, r);
     }
 }
