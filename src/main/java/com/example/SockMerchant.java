ebde762244e7b0b7de961cd5611248cd75c4cfb1
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-09 14:18:50 +0900

    Add SockMerchant

---
+++ src/main/java/com/example/SockMerchant.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SockMerchant {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        HashMap<Integer, Integer> counter = new HashMap<>();
+        int total = 0;
+        for (int a : ar) {
+            int count = counter.getOrDefault(a, 0) + 1;
+            if (count % 2 == 0) {
+                total++;
+            }
+            counter.put(a, count);
+        }
+        return total;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
