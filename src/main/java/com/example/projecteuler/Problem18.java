a1de7dba7b24eb7d9c619847f98b9e88c24356b3
Yuki Hirai <yukihirai0505@gmail.com>
2019-03-12 15:27:08 +0900

    Add Problem18

---
+++ src/main/java/com/example/projecteuler/Problem18.java
@@ -0,0 +1,38 @@
+package com.example.projecteuler;
+
+import java.util.*;
+
+public class Problem18 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static int maxSum(int tri[][]) {
+        int m = tri.length - 1;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                if (tri[i + 1][j] > tri[i + 1][j + 1])
+                    tri[i][j] += tri[i + 1][j];
+                else
+                    tri[i][j] += tri[i + 1][j + 1];
+            }
+        }
+        return tri[0][0];
+    }
+
+    public static void main(String[] args) {
+        int T = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+            int H = scanner.nextInt();
+            scanner.nextLine();
+            int[][] tri = new int[H][H];
+            for (int j = 0; j < H; j++) {
+                String[] next = scanner.nextLine().split(" ");
+                for (int k = 0; k < next.length; k++) {
+                    tri[j][k] = Integer.parseInt(next[k]);
+                }
+            }
+            System.out.println(maxSum(tri));
+        }
+        scanner.close();
+    }
+}
