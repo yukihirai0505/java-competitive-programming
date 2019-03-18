c60712265c5c0d01a339ffc18c0ef5475e93059c
Yuki Hirai <yukihirai0505@gmail.com>
2019-03-18 15:38:09 +0900

    Update QueensAttack

--- src/main/java/com/example/QueensAttack.java
+++ src/main/java/com/example/QueensAttack.java
@@ -12,97 +12,119 @@ public class QueensAttack {
      */
     private static int queensAttack(int n, int k, int rQueen, int cQueen, int[][] obstacles) {
         // Row Column coordinates of the closes object in each direction
-        int rRObstacle = -1;
-        int cRObstacle = -1;
-        int rBRObstacle = -1;
-        int cBRObstacle = -1;
-        int rBObstacle = -1;
-        int cBObstacle = -1;
-        int rBLObstacle = -1;
-        int cBLObstacle = -1;
-        int rLObstacle = -1;
-        int cLObstacle = -1;
-        int rTLObstacle = -1;
-        int cTLObstacle = -1;
-        int rTObstacle = -1;
-        int cTObstacle = -1;
-        int rTRObstacle = -1;
-        int cTRObstacle = -1;
+
+        // Right
+        int rR = -1;
+        int cR = -1;
+
+        // Bottom Right
+        int rBR = -1;
+        int cBR = -1;
+
+        // Bottom
+        int rB = -1;
+        int cB = -1;
+
+        // Bottom Left
+        int rBL = -1;
+        int cBL = -1;
+
+        // Left
+        int rL = -1;
+        int cL = -1;
+
+        // Top Left
+        int rTL = -1;
+        int cTL = -1;
+
+        // Top
+        int rT = -1;
+        int cT = -1;
+
+        // Top Right
+        int rTR = -1;
+        int cTR = -1;
 
         // Total squares attacked by the queen
         int reachableSquares = 0;
 
         // Finds the closest object in each direction
         for (int[] obstacle : obstacles) {
-            int rObstacle = obstacle[0];
-            int cObstacle = obstacle[1];
+            int row = obstacle[0];
+            int column = obstacle[1];
 
-            //　Right
-            if ((cObstacle < cRObstacle || rRObstacle == -1) && cObstacle > cQueen && rObstacle == rQueen) {
-                rRObstacle = rObstacle;
-                cRObstacle = cObstacle;
+            // Right
+            if ((column < cR || rR == -1) && column > cQueen && row == rQueen) {
+                rR = row;
+                cR = column;
             }
 
-            //　Bottom Right
-            if (rQueen - rObstacle == cObstacle - cQueen && cObstacle > cQueen && rObstacle < rQueen
-                    && ((rObstacle > rBRObstacle && cObstacle < cBRObstacle) || rBRObstacle == -1)) {
-                rBRObstacle = rObstacle;
-                cBRObstacle = cObstacle;
+            // Bottom Right
+            if (rQueen - row == column - cQueen && column > cQueen && row < rQueen
+                    && ((row > rBR && column < cBR) || rBR == -1)) {
+                rBR = row;
+                cBR = column;
             }
 
             // Bottom
-            if ((rObstacle > rBObstacle || rBObstacle == -1) && rObstacle < rQueen && cObstacle == cQueen) {
-                rBObstacle = rObstacle;
-                cBObstacle = cObstacle;
+            if ((row > rB || rB == -1) && row < rQueen && column == cQueen) {
+                rB = row;
+                cB = column;
             }
 
             // Bottom Left
-            if (rQueen - rObstacle == cQueen - cObstacle && cObstacle < cQueen && rObstacle < rQueen
-                    && ((rObstacle > rBLObstacle && cObstacle > cBLObstacle) || rBLObstacle == -1)) {
-                rBLObstacle = rObstacle;
-                cBLObstacle = cObstacle;
+            if (rQueen - row == cQueen - column && column < cQueen && row < rQueen
+                    && ((row > rBL && column > cBL) || rBL == -1)) {
+                rBL = row;
+                cBL = column;
             }
 
             // Left
-            if ((cObstacle > cLObstacle || rLObstacle == -1) && cObstacle < cQueen && rObstacle == rQueen) {
-                rLObstacle = rObstacle;
-                cLObstacle = cObstacle;
+            if ((column > cL || rL == -1) && column < cQueen && row == rQueen) {
+                rL = row;
+                cL = column;
             }
 
             // Top Left
-            if (cQueen - cObstacle == rObstacle - rQueen && cObstacle < cQueen && rObstacle > rQueen
-                    && ((rObstacle < rTLObstacle && cObstacle > cTLObstacle) || rTLObstacle == -1)) {
-                rTLObstacle = rObstacle;
-                cTLObstacle = cObstacle;
+            if (cQueen - column == row - rQueen && column < cQueen && row > rQueen
+                    && ((row < rTL && column > cTL) || rTL == -1)) {
+                rTL = row;
+                cTL = column;
             }
 
             // Top
-            if ((rObstacle < rTObstacle || rTObstacle == -1) && rObstacle > rQueen && cObstacle == cQueen) {
-                rTObstacle = rObstacle;
-                cTObstacle = cObstacle;
+            if ((row < rT || rT == -1) && row > rQueen && column == cQueen) {
+                rT = row;
+                cT = column;
             }
 
             // Top Right
-            if (rObstacle - rQueen == cObstacle - cQueen && cObstacle > cQueen
-                    && rObstacle > rQueen && ((rObstacle < rTRObstacle && cObstacle < cTRObstacle) || rTRObstacle == -1)) {
-                rTRObstacle = rObstacle;
-                cTRObstacle = cObstacle;
+            if (row - rQueen == column - cQueen && column > cQueen
+                    && row > rQueen && ((row < rTR && column < cTR) || rTR == -1)) {
+                rTR = row;
+                cTR = column;
             }
 
         }
 
-        //Calculates the distance to the closest obstacle in each direction and adds it to reachableSquares
-        //R B L T
-        reachableSquares += (cRObstacle != -1) ? (cRObstacle - cQueen - 1) : n - cQueen;
-        reachableSquares += (rBObstacle != -1) ? (rQueen - rBObstacle - 1) : rQueen - 1;
-        reachableSquares += (cLObstacle != -1) ? (cQueen - cLObstacle - 1) : cQueen - 1;
-        reachableSquares += (rTObstacle != -1) ? (rTObstacle - rQueen - 1) : n - rQueen;
+        // Calculates the distance to the closest obstacle in each direction and adds it to reachableSquares
+        // Right
+        reachableSquares += (cR != -1) ? (cR - cQueen - 1) : n - cQueen;
+        // Bottom
+        reachableSquares += (rB != -1) ? (rQueen - rB - 1) : rQueen - 1;
+        // Left
+        reachableSquares += (cL != -1) ? (cQueen - cL - 1) : cQueen - 1;
+        // Top
+        reachableSquares += (rT != -1) ? (rT - rQueen - 1) : n - rQueen;
 
-        //BR BL TL TR
-        reachableSquares += (cBRObstacle != -1) ? (cBRObstacle - cQueen - 1) : Math.min(n - cQueen, rQueen - 1);
-        reachableSquares += (rBLObstacle != -1) ? (cQueen - cBLObstacle - 1) : Math.min(cQueen - 1, rQueen - 1);
-        reachableSquares += (cTLObstacle != -1) ? (cQueen - cTLObstacle - 1) : Math.min(cQueen - 1, n - rQueen);
-        reachableSquares += (rTRObstacle != -1) ? (cTRObstacle - cQueen - 1) : Math.min(n - cQueen, n - rQueen);
+        // Bottom Right
+        reachableSquares += (cBR != -1) ? (cBR - cQueen - 1) : Math.min(n - cQueen, rQueen - 1);
+        // Bottom Left
+        reachableSquares += (rBL != -1) ? (cQueen - cBL - 1) : Math.min(cQueen - 1, rQueen - 1);
+        // TopLeft
+        reachableSquares += (cTL != -1) ? (cQueen - cTL - 1) : Math.min(cQueen - 1, n - rQueen);
+        // TopRight
+        reachableSquares += (rTR != -1) ? (cTR - cQueen - 1) : Math.min(n - cQueen, n - rQueen);
         return reachableSquares;
     }
 
